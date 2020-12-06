ab0e25ca1b1247f69ee30666552cd7a42994a009
matbelcao <10503336@polimi.it>
2020-12-06 19:01:14 +0100

    Update example: anomaly detection on pizzas (with timestamp)

--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 
+
 ## HOW TO RUN
 
 Comment/uncomment the file docker-compose.yml to run either the h2 or spark configuration
@@ -12,48 +13,20 @@ foo@bar:~$ sudo docker-compose -f docker-compose-ontop.yml up
 ## QUERY EXAMPLE (localhost:3030  admin:admin)
 
     PREFIX : <http://www.co-ode.org/ontologies/pizza/pizza.owl#>
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-    SELECT *
+    SELECT ?pizzaID ?outcome
     WHERE {
 
-      ?x :suggestedCookingTemp ?suggestedTemp .
+      ?pizzaType :suggestedCookingTemp ?suggestedTemp; :suggestedCookingDuration ?suggestedDuration .
 
       SERVICE <http://ontop:8080/sparql> {
-        ?pizzaID a ?x; :temperature ?avgTemp.
+        ?pizzaID a ?pizzaType.
+        ?pizzaID :temperature ?avgTemp; :start_cooking ?start; :end_cooking ?end.
       }
 
-      OPTIONAL { FILTER( ?avgTemp < ?suggestedTemp*0.90 || ?avgTemp > ?suggestedTemp*1.25 )
-          BIND("BRUCIATA" AS ?v)
-      }
-      OPTIONAL { FILTER( ?avgTemp >= ?suggestedTemp*0.90 && ?avgTemp <= ?suggestedTemp*1.25 )
-          BIND("BEN COTTA" AS ?v)
-      }
+      BIND ((?end-?start) AS ?cookingDuration)
 
-    }
-
-Old query....to be checked
-
-
-    PREFIX : <http://www.co-ode.org/ontologies/pizza/pizza.owl#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-
-    SELECT ?pizzaType ?sensor ?value ?time
-    WHERE {
-        ?pizzaType rdfs:subClassOf  [ owl:onProperty      :hasTopping ;
-                                      owl:someValuesFrom  :MozzarellaTopping ] ;
-                   rdfs:subClassOf  [ owl:onProperty      :hasTopping ;
-                                      owl:someValuesFrom  :OliveTopping ] ;
-                   rdfs:label ?l .
-        SERVICE <http://ontop:8080/sparql> {
-            ?pizzaID :hasSensorMeasure [:value ?value; :capturedAt ?time; :detectedBy ?sensor];
-                      rdfs:label ?l .
-        }
-        BIND ((now() - "P45DT4H"^^xsd:dayTimeDuration) AS ?duration)
-        FILTER( ?time <= now() && ?time > ?duration )
+      BIND(IF ((?avgTemp >= ?suggestedTemp*0.90 && ?avgTemp <= ?suggestedTemp*1.25) &&
+          	   (?cookingDuration >= ?suggestedDuration*0.80 && ?cookingDuration <= ?suggestedDuration*1.40)
+               ,"WELL_COOKED","ANOMALY") AS ?outcome)
     }
--- spark/pizzaDB.sql
+++ spark/pizzaDB.sql
@@ -6,42 +6,42 @@ CREATE TABLE pizzaDB.pizza (
            pizzaID STRING NOT NULL,
            menuName STRING NOT NULL);
 
-CREATE TABLE pizzaDB.measures (
+CREATE TABLE pizzaDB.restaurant1 (
            pizzaID STRING NOT NULL,
-           value DECIMAL(4,1) NOT NULL);
+           temp DECIMAL(4,1) NOT NULL,
+           timestamp TIMESTAMP NOT NULL);
+           
+CREATE TABLE pizzaDB.restaurant2 (
+           pizzaID STRING NOT NULL,
+           temp DECIMAL(4,1) NOT NULL,
+           timestamp TIMESTAMP NOT NULL);
 
 INSERT INTO pizzaDB.pizza
-VALUES ('PZ123','Margherita'),('PZ222','Margherita'),('PZ333','FourSeasons'),('PZ444','Margherita'),('PZ456','Margherita'),('PZ789','FourSeasons');
+VALUES ('PZ123','Margherita'),('PZ456','Margherita'),('PZ789','FourSeasons'),('PZ111','Margherita'),('PZ222','Margherita'),('PZ333','FourSeasons');
 
-INSERT INTO pizzaDB.measures
-VALUES ('PZ123',250.5),('PZ222',273.0),('PZ333',380.0),('PZ333',10.0),('PZ333',20.0),('PZ444',254.7),('PZ456',270.2),('PZ789',260.9);
+INSERT INTO pizzaDB.restaurant1
+VALUES ('PZ123',250.5,CAST('2020-12-05 16:24:46.15' AS Timestamp)),
+       ('PZ123',255.5,CAST('2020-12-05 16:25:46.15' AS Timestamp)),
+       ('PZ123',255.9,CAST('2020-12-05 16:26:46.15' AS Timestamp)),
+       ('PZ123',260.0,CAST('2020-12-05 16:27:46.15' AS Timestamp)),
+       ('PZ456',280.5,CAST('2020-12-05 16:54:55.15' AS Timestamp)),
+       ('PZ456',290.5,CAST('2020-12-05 16:55:55.15' AS Timestamp)),
+       ('PZ456',292.2,CAST('2020-12-05 16:56:55.15' AS Timestamp)),
+       ('PZ456',291.0,CAST('2020-12-05 16:57:55.15' AS Timestamp)),
+       ('PZ789',270.5,CAST('2020-12-05 17:38:47.15' AS Timestamp)),
+       ('PZ789',280.5,CAST('2020-12-05 17:39:47.15' AS Timestamp));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+INSERT INTO pizzaDB.restaurant2
+VALUES ('PZ111',290.5,CAST('2020-12-05 16:24:46.15' AS Timestamp)),
+       ('PZ111',289.5,CAST('2020-12-05 16:25:46.15' AS Timestamp)),
+       ('PZ111',288.9,CAST('2020-12-05 16:26:46.15' AS Timestamp)),
+       ('PZ111',292.0,CAST('2020-12-05 16:27:46.15' AS Timestamp)),
+       ('PZ222',290.5,CAST('2020-12-05 16:28:55.15' AS Timestamp)),
+       ('PZ222',290.5,CAST('2020-12-05 16:29:55.15' AS Timestamp)),
+       ('PZ222',292.2,CAST('2020-12-05 16:30:55.15' AS Timestamp)),
+       ('PZ222',291.0,CAST('2020-12-05 16:31:55.15' AS Timestamp)),
+       ('PZ333',275.5,CAST('2020-12-05 17:45:26.15' AS Timestamp)),
+       ('PZ333',280.5,CAST('2020-12-05 17:46:26.15' AS Timestamp)),
+       ('PZ333',278.5,CAST('2020-12-05 17:47:26.15' AS Timestamp)),
+       ('PZ333',277.5,CAST('2020-12-05 17:48:26.15' AS Timestamp));
 
